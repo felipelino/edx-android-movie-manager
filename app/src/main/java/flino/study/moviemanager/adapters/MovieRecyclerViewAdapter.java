1926bb526a355407f154f7bf63a0f748a7887211
Felipe Lino <felipelino44@gmail.com>
2017-08-04 08:55:35 -0300

    Include Recyclerview and Adapter for Movies

---
+++ app/src/main/java/flino/study/moviemanager/adapters/MovieRecyclerViewAdapter.java
@@ -0,0 +1,78 @@
+package flino.study.moviemanager.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import flino.study.moviemanager.R;
+import flino.study.moviemanager.models.Movie;
+
+/**
+ * Created by felipe.lino on 8/4/17.
+ */
+
+public class MovieRecyclerViewAdapter extends RecyclerView.Adapter<MovieRecyclerViewAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Movie> movies;
+
+    public MovieRecyclerViewAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_movie, parent, false);
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Movie movie = this.movies.get(position);
+
+        holder.tvTitle.setText(movie.getTitle());
+        holder.tvOverview.setText(movie.getOverview());
+
+        Picasso.with(getContext())
+                .load(movie.getPosterPath())
+                .into(holder.ivMovieImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.movies.size();
+    }
+
+    private Context getContext() {
+        return this.context;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.ivMovieImage)
+        ImageView ivMovieImage;
+        @BindView(R.id.tvTitle)
+        TextView tvTitle;
+        @BindView(R.id.tvOverview)
+        TextView tvOverview;
+        @BindView(R.id.cvMovie)
+        CardView cvMovie;
+
+        ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+}
