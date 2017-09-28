2e6dcd0467ea839e542f08dca4b1c1f1760c294f
Felipe Lino <felipe.lino@b2wdigital.com>
2017-09-28 08:32:35 -0300

    Add test with Espresso

--- app/src/androidTest/java/flino/study/moviemanager/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/flino/study/moviemanager/ExampleInstrumentedTest.java
@@ -2,11 +2,19 @@ package flino.study.moviemanager;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import flino.study.moviemanager.activities.MainActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -23,4 +31,21 @@ public class ExampleInstrumentedTest {
 
         assertEquals("flino.study.moviemanager", appContext.getPackageName());
     }
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule
+            = new ActivityTestRule<MainActivity>(MainActivity.class, true, true) {};
+
+    @Test
+    public void validateRecyclerViewClickSecondElement() {
+        onView(withId(R.id.rvMovies))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
+    }
+
+    @Test
+    public void validateRecyclerViewClickThirdElement() {
+        onView(withId(R.id.rvMovies))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(3, click()));
+    }
+
 }
